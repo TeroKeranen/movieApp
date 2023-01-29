7b1f9cdad0c53456b940daaca9984f31bd2feb57
Tero Keränen <tero-keranen@hotmail.com>
2023-01-29 19:06:47 +0200

    debugging heroku

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ app.get('/register', (req,res) => {
 
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, function () {
     console.log(`server started on port ${PORT}`);
